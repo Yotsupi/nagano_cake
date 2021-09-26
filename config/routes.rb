e2a7421b97ebe560be5737cc847a9a1f1eb46274
Yotsupi <kayoshida.1830@gmail.com>
2021-09-26 13:31:31 +0000

    [Add]top,about

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'public/homes#top'
+  get "/about" => "public/homes#about"
+  namespace :public do
+
+  end
 end
