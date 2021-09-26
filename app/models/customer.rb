a8a2495b62fb73d49fc3673366067787e3c6bfd0
Yotsupi <kayoshida.1830@gmail.com>
2021-09-26 08:27:26 +0000

    [Add]AddressModel

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :postal_code, numericality: true
   validates :telephone_number, numericality: true
+
+  has_many :addresses, dependent :destroy
 end
