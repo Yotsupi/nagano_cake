c7b49247eed9e6593520cc4d5dccb5837af8145e
Yotsupi <kayoshida.1830@gmail.com>
2021-09-27 11:58:47 +0000

    [Add]商品編集ページ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,10 +18,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
-    
+    @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to new_admin_item_path
   end
 
   private
